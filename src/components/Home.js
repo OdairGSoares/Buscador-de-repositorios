9f14588d7a1823d84cf2526e9d9ad03bf07fa83c
OdairGSoares <contatoodairgomes@gmail.com>
2022-06-19 12:53:27 -0300

    atualizando_interface

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,75 @@
 import React, { useState } from "react";
 import axios from "axios";
-import {useNavigate} from 'react-router-dom';
-export default function Home(props){
+import { useNavigate } from "react-router-dom";
+export default function Home(props) {
+  const navigate = useNavigate();
+  const [usuario, setUsuario] = useState("");
+  const [erro, setErro] = useState("");
 
-    const navigate = useNavigate();
-    const [usuario, setUsuario] = useState('');
-    const [erro, setErro] = useState('');
-  
-    function handlePesquisa(){
-      localStorage.clear();
-      axios.get(`https://api.github.com/users/${usuario}/repos`)
-      .then(response => {
-        
-        if(response!=[]){
-        const repositories = response.data;
-        const repositories_names = [];
-        repositories.map((repository)=>{
-          repositories_names.push(repository.name);
-        })
+  function handlePesquisa(e) {
+    e.preventDefault();
+    localStorage.clear();
+    axios
+      .get(`https://api.github.com/users/${usuario}/repos`)
+      .then((response) => {
+        if (response != []) {
+          const repositories = response.data;
+          const repositories_names = [];
+          repositories.map((repository) => {
+            repositories_names.push(repository.name);
+          });
 
-        
-        localStorage.setItem('repositories_names', JSON.stringify(repositories_names));
-        navigate('/repositories')
-        setErro('')
-        }else{
-          
-        } 
-      }).catch((err) => {
-
-        setErro('usuario não encontrado!')
+          localStorage.setItem(
+            "repositories_names",
+            JSON.stringify(repositories_names)
+          );
+          navigate("/repositories");
+          setErro("");
+        } else {
+        }
+      })
+      .catch((err) => {
+        setErro("usuario não encontrado!");
       });
-    }
-  
-    return(
-        <section>
-            <p className="texto">{usuario}</p>
-            <input placeholder='usuario' className="usuario_input" value={usuario} onChange={e => setUsuario(e.target.value)} />
-            <button type="button" onClick={handlePesquisa} >Pesquisar</button>
-            <p id="resultado"></p>
-            <p >{erro}</p>
-        </section>
-    )
   }
+
+  return (
+    <section>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Git Search
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Coloque o nome do usuário"
+                aria-label="Search"
+                value={usuario}
+                onChange={(e) => setUsuario(e.target.value)}
+              />
+              <button className="btn btn-outline-success" type="submit" onClick={(e)=>{handlePesquisa(e)}}>
+                Pesquisar repositórios
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <p id="resultado"></p>
+      <p>{erro}</p>
+    </section>
+  );
+}
--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,28 +1,33 @@
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Repositories() {
-
   const [repositories, set_repositories] = useState([]);
 
   useEffect(() => {
-    let repositories_names = localStorage.getItem('repositories_names');
+    let repositories_names = localStorage.getItem("repositories_names");
     repositories_names = JSON.parse(repositories_names);
     set_repositories(repositories_names);
-    }, []);
-  
+  }, []);
+
   return (
-    <>
+    <div className="row text-center">
       <ul>
-          {repositories.map(repository => {
-            return (
-              <li>{repository}</li>
-            )
-          })}
-        <Link to='/'>Voltar</Link>
+        {repositories.map((repository) => {
+          return (
+            <h6 className="text-center mt-5">
+              <i className="fa-solid fa-circle-dot fa-2xs"></i> {repository}
+            </h6>
+          );
+        })}
       </ul>
-    </>
+      <div className="row ms-1 text-center">
+        <Link to="/">
+          <button type="button" className="btn btn-outline-primary">
+            Voltar
+          </button>
+        </Link>
+      </div>
+    </div>
   );
-
-
 }
